51fde405c930aada5acb9d0671961667317f464d
Margaret Mullooly <mmullooly@mail.fresnostate.edu>
2026-02-04 10:17:59 -0800

    fix link

--- components/About.jsx
+++ components/About.jsx
@@ -1,4 +1,5 @@
 import "../styles/about.css"
+import { Link } from "react-router-dom";
 
 export default function About(){
 
--- components/Playtests.jsx
+++ components/Playtests.jsx
@@ -1,5 +1,6 @@
 import "../styles/playtests.css"
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const base = import.meta.env.BASE_URL;
 
